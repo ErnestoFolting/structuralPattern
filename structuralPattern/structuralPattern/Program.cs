4f29531b3d5b8daff5900db91eded7595f48016c
Danil <danilsmy.edu@gmail.com>
2022-05-28 01:27:40 +0300

    Add an empty project

---
+++ structuralPattern/structuralPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structuralPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
